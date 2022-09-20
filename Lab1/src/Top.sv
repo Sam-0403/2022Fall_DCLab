34434d9ff74fd3240b6a37c7ac6e74b8abb04de9
Clara Chu <clarachu2002@gmail.com>
2022-09-20 15:03:51 +0800

    Basic functionality implementation

--- Lab1/src/Top.sv
+++ Lab1/src/Top.sv
@@ -18,9 +18,11 @@ logic [3:0] o_random_out_r, o_random_out_w;
 
 // Regs & Wires for FSM
 logic state_r, state_w;
+
 // Regs & Wires for Counter Comparator
 logic [31:0] counter_r, counter_w;
 logic [31:0] compare_r, compare_w;
+
 // Regs & Wires for LFSR
 // http://www.xilinx.com/support/documentation/application_notes/xapp052.pdf
 logic [9:0] LFSR_r = 10'b0, LFSR_w;
@@ -33,21 +35,50 @@ always_comb begin
 	// Default Values
 	o_random_out_w = o_random_out_r;
 	state_w        = state_r;
+    counter_w      = counter_r;
+    compare_w      = compare_r;
+    LFSR_w         = LFSR_r;
 
 	// FSM
 	case(state_r)
 	S_IDLE: begin
 		if (i_start) begin
-			state_w = S_PROC;
-			o_random_out_w = 4'd15;
+			state_w         = S_PROC;
+			o_random_out_w  = {LFSR_r[3], LFSR_r[2], LFSR_r[1], LFSR_r[0]};
+            counter_w       = 32'b0;
+            compare_w       = 32'b1000_0000_0000;
 		end
 	end
 
 	S_PROC: begin
+
+        
+
 		if (i_start) begin
-			state_w = (o_random_out_r == 4'd10) ? S_IDLE : state_w;
-			o_random_out_w = (o_random_out_r == 4'd10) ? 4'd1 : (o_random_out_r - 4'd1);
+			// TODO: 截取亂數
 		end
+
+        else if (counter_r == compare_r) begin
+            state_w         = S_PROC;
+            LFSR_w          = {~(LFSR_r[0]^LFSR_r[3]), LFSR_r[9], LFSR_r[8], LFSR_r[7], LFSR_r[6], 
+                                LFSR_r[5], LFSR_r[4], LFSR_r[3], LFSR_r[2], LFSR_r[1]};
+            o_random_out_w  = {LFSR_r[3], LFSR_r[2], LFSR_r[1], LFSR_r[0]};
+            counter_w       = counter_r + 1'b1;
+            compare_w       = compare_w << 1;
+        end
+
+        else if (counter_r == 32'b111_1111_1111_1111_1111_1111_1111) begin
+            state_w         = S_IDLE;
+            LFSR_w          = {~(LFSR_r[0]^LFSR_r[3]), LFSR_r[9], LFSR_r[8], LFSR_r[7], LFSR_r[6], 
+                                LFSR_r[5], LFSR_r[4], LFSR_r[3], LFSR_r[2], LFSR_r[1]};
+            o_random_out_w  = {LFSR_r[3], LFSR_r[2], LFSR_r[1], LFSR_r[0]};             
+        end
+
+        else begin
+            state_w         = S_PROC;
+            o_random_out_w  = {LFSR_r[3], LFSR_r[2], LFSR_r[1], LFSR_r[0]};
+            counter_w       = counter_r + 1'b1;
+        end
 	end
 
 	endcase
@@ -59,10 +90,17 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin
 	if (!i_rst_n) begin
 		o_random_out_r <= 4'd0;
 		state_r        <= S_IDLE;
+        counter_r      <= 32'b0;
+        compare_r      <= 32'b1000_0000_0000;
+        LFSR_r         <= 10'b0;
 	end
+
 	else begin
 		o_random_out_r <= o_random_out_w;
 		state_r        <= state_w;
+        counter_r      <= counter_w;
+        compare_r      <= compare_w;
+        LFSR_r         <= LFSR_w;
 	end
 end
 
