12b05adcc9cdf24d6c1abcea0b529683a76e5898
鄭至盛 <sam22187212@gmail.com>
2022-09-25 13:37:12 +0800

    Add stored value TB

--- Lab1/sim/tb_Top.sv
+++ Lab1/sim/tb_Top.sv
@@ -6,7 +6,10 @@ parameter	cycle = 100.0;
 
 logic 		i_clk;
 logic 		i_rst_n, i_start;
+logic 		i_control;
+logic 		i_index_0, i_index_1, i_index_2, i_index_3;
 logic [3:0] o_random_out;
+logic [3:0] o_stored_out;
 
 initial i_clk = 0;
 always #(cycle/2.0) i_clk = ~i_clk;
@@ -15,7 +18,13 @@ Top top0(
 	.i_clk(i_clk),
 	.i_rst_n(i_rst_n),
 	.i_start(i_start),
-	.o_random_out(o_random_out)
+	.i_control(i_control),
+	.i_index_0(i_index_0),
+	.i_index_1(i_index_1),
+	.i_index_2(i_index_2),
+	.i_index_3(i_index_3),
+	.o_random_out(o_random_out),
+	.o_stored_out(o_stored_out)
 );
 
 initial begin
@@ -27,6 +36,12 @@ initial begin
 	i_clk 	= 0;
 	i_rst_n = 1;
 	i_start	= 0;
+
+	i_control = 0;
+	i_index_0 = 1;
+	i_index_1 = 0;
+	i_index_2 = 0;
+	i_index_3 = 0;
 
 	@(negedge i_clk);
 	@(negedge i_clk);
@@ -44,6 +59,10 @@ initial begin
 	@(negedge i_clk) i_start = 1;
 	@(negedge i_clk);
 	@(negedge i_clk) i_start = 0;
+	#32000000
+	@(negedge i_clk) i_control = 1;
+	@(negedge i_clk);
+	@(negedge i_clk) i_control = 0;
 end
 
 initial #(cycle*10000000) $finish;
