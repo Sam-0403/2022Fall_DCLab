4999827b8cf8d5ff5f64b8813535761a72f7728c
鄭至盛 <sam22187212@gmail.com>
2022-10-10 14:22:34 +0800

    Modify python

--- Lab2/src/pc_python/rs232.py
+++ Lab2/src/pc_python/rs232.py
@@ -15,13 +15,13 @@ s = Serial(
 for i in range(3):
     fp_key = open(f'./golden/key{i+1}.bin', 'rb')
     fp_enc = open(f'./golden/enc{i+1}.bin', 'rb')
-    fp_dec = open(f'./golden/dec{i+1}.bin', 'wb')
+    fp_dec = open(f'./test/dec{i+1}.bin', 'wb')
     assert fp_key and fp_enc and fp_dec
 
     key = fp_key.read(64)
     enc = fp_enc.read()
     # finish signal: key
-    enc += key[0:31]
+    enc += key[0:31]    # add FINISH signal
     assert len(enc) % 32 == 0
 
     s.write(key)
@@ -29,8 +29,6 @@ for i in range(3):
         s.write(enc[i:i+32])
         dec = s.read(31)
         fp_dec.write(dec)
-
-    s.write(key)
 
     fp_key.close()
     fp_enc.close()
