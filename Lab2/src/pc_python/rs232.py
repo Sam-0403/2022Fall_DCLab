9b1619a6bcca106eb6abd4a250cbd71705aff10c
鄭至盛 <sam22187212@gmail.com>
2022-10-12 11:05:45 +0800

    Final Version

--- Lab2/src/pc_python/rs232.py
+++ Lab2/src/pc_python/rs232.py
@@ -13,13 +13,12 @@ s = Serial(
     rtscts=False
 )
 while True:
-    # print("Index: {index}".format(index=j+1))
     try:
-        type_enc = input("Enter the encrypted type (0:RS232/1:RailFence): ")
+        type_enc = int(input("Enter the encrypted type (0:RS232/1:RailFence): "))
         idx = input("Enter the encrypted file index: ")
         fp_key = open('./golden/key.bin', 'rb')
         fp_dec = open('./test/dec{index}.bin'.format(index=idx), 'wb')
-        if type_enc=="1":
+        if type_enc==1:
             fp_enc = open('./railfence/enc{index}.bin'.format(index=idx), 'rb')
         else:
             fp_enc = open('./golden/enc{index}.bin'.format(index=idx), 'rb')
@@ -27,22 +26,15 @@ while True:
 
         key = fp_key.read(64)
         enc = fp_enc.read()
-        # print(len(key))
-        # print(len(enc))
         enc += key    # add FINISH signal
         assert len(enc) % 32 == 0
-        # print(len(enc))
 
         s.write(key)
         for i in range(0, len(enc)-32, 32):
             s.write(enc[i:i+32])
-            # print("Dec start")
             if i<len(enc)-64:
                 dec = s.read(31)
-                # print("Dec end")
                 fp_dec.write(dec)
-            # else:
-                # print("Finish signal sent")
 
         fp_key.close()
         fp_enc.close()
