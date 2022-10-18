c3bc2b40fee194216c6313aba075e3a8a56e2aa4
鄭至盛 <sam22187212@gmail.com>
2022-10-18 23:27:23 +0800

    Lab3: AudRecorder

--- Lab3/src/AudRecorder.sv
+++ Lab3/src/AudRecorder.sv
@@ -11,24 +11,103 @@ module AudRecorder(
 );
 
 // ===== States =====
+localparam S_IDLE   = 2'd0;
+localparam S_WAIT   = 2'd1;
+localparam S_DATA   = 2'd2;
+localparam S_PAUSE  = 2'd3;
 
 // ===== Constants =====
+localparam BASE = 20'b11111111111111111111; // BASE0 = -1
 
 // ===== Registers & Wires =====
+logic [1:0] state_r, state_w;
+logic [4:0] counter_r, counter_w;   // bit counter for 16 bit audio
+logic [19:0] addr_r, addr_w;
+logic [15:0] data_r, data_w;
 
 // ===== Output Assignments =====
+assign o_address    = addr_r;
+assign o_data       = data_r;
 
 // ===== Combinational Circuits =====
 always_comb begin
     // Default Values
+    state_w     = state_r;
+    counter_w   = counter_r;
+    addr_w      = addr_r;
+    data_w      = data_r;
     // FSM
+    case(state_r)
+        S_IDLE: begin
+            if(i_start) begin
+                state_w = S_WAIT;
+                addr_w  = BASE;
+            end
+            counter_w   = 5'd0;
+			data_w      = 16'd0;
+        end
+        S_WAIT: begin
+            if(i_stop) begin
+                state_w = S_IDLE;
+            end
+            else if(i_pause) begin
+                state_w = S_PAUSE;
+            end
+            else if(i_lrc) begin //record right channel
+                state_w = S_DATA;
+            end
+            counter_w   = 5'd0;
+            data_w      = 16'd0;
+        end
+        S_DATA: begin
+            if(i_stop) begin
+                state_w     = S_IDLE;
+                counter_w   = 5'd0;
+                addr_w      = addr_r;
+                data_w      = 16'd0;
+            end
+            else if(i_pause) begin
+                state_w     = S_PAUSE;
+                counter_w   = 5'd0;
+                addr_w      = addr_r;
+                data_w      = 16'd0;
+            end
+            else if(Bctr<5'd16) begin //record right channel
+                state_w     = S_DATA;
+                counter_w   = counter_r + 5'd1;
+                addr_w      = addr_r;
+                data_w      = {data_r[15:0],i_data};
+            end
+            else if(!i_lrc) begin
+                state_w     = S_WAIT;
+                counter_w   = 5'd0;
+                addr_w      = addr_r + 20'd1; //only change o_address when output(i_lrc=1=right channel)
+                data_w      = data_r;
+            end
+        end
+        S_PAUSE: begin
+            if(i_start) begin
+                state_w = S_WAIT;
+            end
+            counter_w   = 5'd0;
+            data_w      = 16'd0;
+        end
+    endcase
 end
 
 // ===== Sequential Circuits =====
 always_ff @(posedge i_clk or negedge i_rst_n) begin
     if (!i_rst_n) begin
+        state_r     <= S_IDLE;
+        counter_r   <= 5'd0;
+        addr_r      <= BASE;
+        data_r      <= 16'd0;
     end
     else begin
+        state_r     <= state_w;
+        counter_r   <= counter_w;
+        addr_r      <= addr_w;
+        data_r      <= data_w;
     end
 end
 
--- Lab3/src/Top.sv
+++ Lab3/src/Top.sv
@@ -72,13 +72,16 @@ assign o_SRAM_UB_N = 1'b0;
 // below is a simple example for module division
 // you can design these as you like
 
+// ===== Registers & Wires =====
+logic i2c_start, i2c_finish;
+
 // === I2cInitializer ===
 // sequentially sent out settings to initialize WM8731 with I2C protocal
 I2cInitializer init0(
 	.i_rst_n(i_rst_n),
 	.i_clk(i_clk_100K),
-	.i_start(),
-	.o_finished(),
+	.i_start(i2c_start),
+	.o_finished(i2c_finish),
 	.o_sclk(o_I2C_SCLK),
 	.o_sdat(i2c_sdat),
 	.o_oen(i2c_oen) // you are outputing (you are not outputing only when you are "ack"ing.)
