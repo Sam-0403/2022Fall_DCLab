606247c6883883e6f995a526b36ada32c7aa0fc2
鄭至盛 <sam22187212@gmail.com>
2022-11-26 00:30:50 +0800

    Failed Code

--- lab_bonus_SW/src/SW_Core_v2.sv
+++ lab_bonus_SW/src/SW_Core_v2.sv
@@ -257,17 +257,17 @@ module SW_core(
                 //dp loop
                 counter_n                                                               = (counter == seq_A_length + seq_B_length - 1) ? 0 : counter + 1;
                 //stop updating
-                for (i=0;i<`READ_MAX_LENGTH;i=i+1) sequence_B_valid_n[i]                = (counter>=seq_A_length+i) ? 0 : 1;
+                for (i=0;i<`READ_MAX_LENGTH;i=i+1) sequence_B_valid_n[i]                = (counter>=seq_A_length+i || counter<i) ? 0 : 1;
                 sequence_A_shifter_n                                                    = sequence_A_shifter << 2;
 
                 //======== Calculate rows' highest score simutaneously ========//
                 highest_score_n                                                         = 0;
                 column_n                                                                = 0;
                 row_n                                                                   = 0;
-                row_highest_scores_n[0]                                                 = PE_score_buff_n[0];
-                for (i=1;i<`READ_MAX_LENGTH;i=i+1) row_highest_scores_n[i]              = (PE_score_buff_n[i]>row_highest_scores[i]) ? PE_score_buff_n[i] : row_highest_scores[i];
-                row_highest_columns_n[0]                                                = 0;
-                for (i=1;i<`READ_MAX_LENGTH;i=i+1) row_highest_columns_n[i]             = (PE_score_buff_n[i]>row_highest_scores[i]) ? i+1 : row_highest_columns[i];
+                // row_highest_scores_n[0]                                                 = PE_score_buff_n[0];
+                for (i=0;i<`READ_MAX_LENGTH;i=i+1) row_highest_scores_n[i]              = (PE_score_buff_n[i]>row_highest_scores[i]) ? PE_score_buff_n[i] : row_highest_scores[i];
+                // row_highest_columns_n[0]                                                = 0;
+                for (i=0;i<`READ_MAX_LENGTH;i=i+1) row_highest_columns_n[i]             = (PE_score_buff_n[i]>row_highest_scores[i]) ? i : row_highest_columns[i];
                 //======== Keep track of column # of the highest scores of each row ========//
 
                 for (i=0;i<`READ_MAX_LENGTH;i=i+1) PE_align_score_d_n  [i]              = PE_align_score    [i];
